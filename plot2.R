16e11f526cf0d08c16736d23d45ff277df7f987a
Marcos <123852633+MarcosEJV@users.noreply.github.com>
2025-06-21 16:46:20 -0400

    Update plot2.R
    Correcting the x-axis

--- plot2.R
+++ plot2.R
@@ -14,6 +14,8 @@ targetdates$DateTime <- strptime(dateandtime, "%d/%m/%Y %H:%M:%S") #Builds a new
  
 png("plot2.png", width = 480, height = 480, units = "px") #Opens the png file
 plot(targetdates$DateTime,targetdates$Global_active_power, type = "l", xlab = "", #Builds the graph
-     ylab = "Global Active Power (kilowatts)")
+     ylab = "Global Active Power (kilowatts)", xaxt = "n") #Deletes the x-axis
+axis(side = 1, at = as.POSIXct(c("2007-02-01 00:00:00", "2007-02-02 00:00:00", #Adds 2007-02-01, 2007-02-02 00:00:00 and 2007-02-02 23:59:00 as label points
+                       "2007-02-02 23:59:00")),labels = c("Thu", "Fri", "Sat")) #New label as "Thu, Fri and Sat"
 
 dev.off() #Closes the file

2c3551bc1b206d22d19b56861c67308c369ba757
Marcos <123852633+MarcosEJV@users.noreply.github.com>
2025-06-21 15:40:10 -0400

    Create plot3.R
    Code to build plot3 for the Exploratory Data Analysis Project 1 from Coursera.

---
+++ plot3.R
@@ -0,0 +1,23 @@
+library(dplyr) #Package used to edit the table
+
+table <- read.csv("./household_power_consumption.txt", sep = ";") #To read the table
+table <- tbl_df(table)
+
+targetdate1 <- table %>% filter(Date == "1/2/2007") #First day subset
+targetdate2 <- table %>% filter(Date == "2/2/2007") #Second day subset
+targetdates <- targetdate1 %>% rbind(targetdate2)   #Merging days
+
+date <- targetdates$Date #Gets date
+time <- targetdates$Time #Gets time
+dateandtime <- paste(date, time) #Paste dates and time into a vector
+targetdates$DateTime <- strptime(dateandtime, "%d/%m/%Y %H:%M:%S") #Builds a new time column to the table
+
+png("plot3.png", width = 480, height = 480, units = "px") #Opens the png file
+with(targetdates, plot(targetdates$DateTime, targetdates$Sub_metering_1,  #Builds the plot based on sub metering 1
+                       col = "black", type = "l", xlab = "", ylab = "Energy sub metering"))
+lines(targetdates$DateTime, targetdates$Sub_metering_2, col = "red", type = "l") #Adds sub metering 2
+lines(targetdates$DateTime, targetdates$Sub_metering_3, col = "blue", type = "l") #Adds sub metering 3
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),  #Adds the legend
+       col = c("black", "red", "blue"), lty = 1)
+
+dev.off() #Closes the png file

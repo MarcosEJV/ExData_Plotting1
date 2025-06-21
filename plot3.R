27f90b80a486a1f7a57c7783a62ee42b2ea33c22
Marcos <123852633+MarcosEJV@users.noreply.github.com>
2025-06-21 16:51:48 -0400

    Update plot3.R
    Correcting the x-axis labels

--- plot3.R
+++ plot3.R
@@ -14,10 +14,12 @@ targetdates$DateTime <- strptime(dateandtime, "%d/%m/%Y %H:%M:%S") #Builds a new
 
 png("plot3.png", width = 480, height = 480, units = "px") #Opens the png file
 with(targetdates, plot(targetdates$DateTime, targetdates$Sub_metering_1,  #Builds the plot based on sub metering 1
-                       col = "black", type = "l", xlab = "", ylab = "Energy sub metering"))
+                       col = "black", type = "l", xlab = "", ylab = "Energy sub metering", xaxt = "n"))
 lines(targetdates$DateTime, targetdates$Sub_metering_2, col = "red", type = "l") #Adds sub metering 2
 lines(targetdates$DateTime, targetdates$Sub_metering_3, col = "blue", type = "l") #Adds sub metering 3
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),  #Adds the legend
        col = c("black", "red", "blue"), lty = 1)
+axis(side = 1, at = as.POSIXct(c("2007-02-01 00:00:00", "2007-02-02 00:00:00", #Adds 2007-02-01, 2007-02-02 00:00:00 and 2007-02-02 23:59:00 as label points
+                                 "2007-02-02 23:59:00")),labels = c("Thu", "Fri", "Sat")) #New label as "Thu, Fri and Sat"
 
 dev.off() #Closes the png file

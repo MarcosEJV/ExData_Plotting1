1ce71d073de4527c5a2956f67bdfc86acc007cdb
Marcos <123852633+MarcosEJV@users.noreply.github.com>
2025-06-21 16:54:49 -0400

    Update plot4.R
    Correcting the x-axis labels

--- plot4.R
+++ plot4.R
@@ -17,19 +17,27 @@ png("plot4.png", width = 480, height = 480, units = "px") #Opens the png file
 par(mfrow = c(2,2)) #Specifies plots organization
 
 plot(targetdates$DateTime,targetdates$Global_active_power, type = "l", xlab = "", #Builds plot 1
-     ylab = "Global Active Power (kilowatts)")
+     ylab = "Global Active Power (kilowatts)", xaxt = "n")
+axis(side = 1, at = as.POSIXct(c("2007-02-01 00:00:00", "2007-02-02 00:00:00", #Adds 2007-02-01, 2007-02-02 00:00:00 and 2007-02-02 23:59:00 as label points
+                                 "2007-02-02 23:59:00")),labels = c("Thu", "Fri", "Sat")) #New label as "Thu, Fri and Sat"
 
-plot(targetdates$DateTime,targetdates$Voltage, type = "l", xlab = "DateTime", ylab = "Voltage") #Builds plot 2
+plot(targetdates$DateTime,targetdates$Voltage, type = "l", xlab = "DateTime", ylab = "Voltage", xaxt = "n") #Builds plot 2
+axis(side = 1, at = as.POSIXct(c("2007-02-01 00:00:00", "2007-02-02 00:00:00",
+                                 "2007-02-02 23:59:00")),labels = c("Thu", "Fri", "Sat"))
 
 with(targetdates, plot(targetdates$DateTime, targetdates$Sub_metering_1, #Builds plot 3
-                       col = "black", type = "l", xlab = "", ylab = "Energy sub metering"))
+                       col = "black", type = "l", xlab = "", ylab = "Energy sub metering", xaxt = "n"))
 lines(targetdates$DateTime, targetdates$Sub_metering_2, col = "red", type = "l")
 lines(targetdates$DateTime, targetdates$Sub_metering_3, col = "blue", type = "l")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        col = c("black", "red", "blue"), lty = 1, bty = "n")
+axis(side = 1, at = as.POSIXct(c("2007-02-01 00:00:00", "2007-02-02 00:00:00",
+                                 "2007-02-02 23:59:00")),labels = c("Thu", "Fri", "Sat"))
 
 plot(targetdates$DateTime,targetdates$Global_reactive_power, type = "l", xlab = "DateTime", #Builds plot 4
-     ylab = "Global_reactive_power")
+     ylab = "Global_reactive_power", xaxt = "n")
+axis(side = 1, at = as.POSIXct(c("2007-02-01 00:00:00", "2007-02-02 00:00:00",
+                                 "2007-02-02 23:59:00")),labels = c("Thu", "Fri", "Sat"))
 
 dev.off() #Closes the png file
 

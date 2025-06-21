9885ee52b00759f7cc23cd9e8ec38c989d7774ee
Marcos <123852633+MarcosEJV@users.noreply.github.com>
2025-06-21 13:27:47 -0400

    Update plot1.R
    Updated the png file name

--- plot1.R
+++ plot1.R
@@ -7,7 +7,7 @@ targetdate1 <- table %>% filter(Date == "1/2/2007") #Read first day
 targetdate2 <- table %>% filter(Date == "2/2/2007") #Read second day
 targetdates <- targetdate1 %>% rbind(targetdate2)   #Bind both days
 
-png("plot1R.png", width = 480, height = 480, units = "px") #Open png file
+png("plot1.png", width = 480, height = 480, units = "px") #Open png file
 hist(as.numeric(targetdates$Global_active_power), col = "red", #Build the figure
      main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off() #Close the figure and save it
